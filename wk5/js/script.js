e529d1500d6c6ed9164bb07eb5aa80737942ba59
jhkivinen <jhkivinen@gmail.com>
2016-07-25 12:41:05 +0300

    wk5 assignment 3rd version

--- wk5/js/script.js
+++ wk5/js/script.js
@@ -154,6 +154,7 @@ dc.loadMenuCategories = function () {
 // Load the menu items view
 // 'categoryShort' is a short_name for a category
 dc.loadMenuItems = function (categoryShort) {
+  console.log(categoryShort);
   showLoading("#main-content");
   $ajaxUtils.sendGetRequest(
     menuItemsUrl + categoryShort,
